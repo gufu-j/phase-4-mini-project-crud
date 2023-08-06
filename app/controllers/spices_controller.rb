a81b8f83d6dd804cfb928b5d57c27d4aa9213888
diego <guatemaladiegojose@gmail.com>
2023-08-06 11:44:12 -0400

    added the get, create and update method successfully

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,42 @@
+class SpicesController < ApplicationController
+
+    # GET/spices
+    def index 
+        spices = Spice.all
+        render json: spices
+    end
+
+    #POST/spices
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: 201
+    end
+
+    #PATCH/spices/:id
+    def update
+        spice = Spice.find_by(id: params[:id])
+        spice.update(spice_params)
+        render json: spice
+    end
+
+
+    private
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    #we could use this method below to clean or code with update method,
+    #we could need to write spice = Spice.find_spice to make it work
+    # but for the purpose of passing this lab is not needed.
+    # def find_spice
+    #     Spice.find_by(id: params[:id])
+    # end
+
+
+    
+
+
+
+
+end
